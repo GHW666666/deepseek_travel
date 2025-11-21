ce379b6b885906428b8b93ebc23cedbe360ba1d9
ghw666666 <3121906711@qq.com>
2025-11-21 23:35:54 +0800

    uploadfile2

--- koa-sever/app.js
+++ koa-sever/app.js
@@ -4,6 +4,8 @@ const json=require('koa-json');//将http请求的body解析为json格式
 const bodyParser=require('koa-bodyparser');//解析post请求的body
 const cors=require('@koa/cors');//解决跨域问题
 const { addAliases } = require('module-alias');//解决路径问题
+const static=require('koa-static');//静态资源中间件
+const path=require('path');//路径模块
 addAliases({
     '@': __dirname, // 根目录别名
 
@@ -16,7 +18,7 @@ const errorHandler = require('@/config/errorhandler');//引入错误中间件
 app.use(cors());//解决跨域问题
 app.use(json());//将http请求的body解析为json格式
 app.use(bodyParser());//解析post请求的body
-
+app.use(static(path.join(__dirname)));//静态资源中间件
 app.use(errorHandler);//使用错误中间件
 app.use(responseHandler);//使用响应中间件
 app.use(router.routes()).use(router.allowedMethods());//使用路由
--- koa-sever/config/uploadfile.js
+++ koa-sever/config/uploadfile.js
@@ -3,9 +3,9 @@ const multer = require('@koa/multer');
 
 const storage = multer.diskStorage({
     destination:  (req, file, cb) =>{ //设置文件存储路径
-        //  console.log(file);
+         console.log(file);
         //校验文件类型
-        const fileType=["image/png","image/jpg","image/webp"  ]//允许的文件类型
+        const fileType=["image/png","image/jpeg","image/webp"  ]//允许的文件类型
         if(!fileType.includes(file.mimetype)){
             //如果文件类型不在允许的文件类型中
             return new Error("文件类型不允许")
@@ -13,8 +13,12 @@ const storage = multer.diskStorage({
         }
         cb(null, 'image_file/'); //存储路径
     },
-    filename: function (req, file, cb) { //设置文件名
-        cb(null, Date.now() + '-' + file.originalname); //文件名
+    filename: function (req, file, cb) { 
+        const filename=file.originalname.split(".")//设置文件名
+        // console.log(filename);
+        const newFileName=Date.now()+"."+filename[1]
+        // console.log(newFileName);
+        cb(null, newFileName); //存储文件名
     }
 });
 const uploadFile = multer({storage }); //创建上传对象
